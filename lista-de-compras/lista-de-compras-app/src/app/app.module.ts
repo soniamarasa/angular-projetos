3e2a1a831f38484b05231315f15bd2a7c4248ab3
soniamarasa <soniamarasa@hotmail.com>
2021-03-15 01:07:20 -0300

    criando componentes

--- lista-de-compras/lista-de-compras-app/src/app/app.module.ts
+++ lista-de-compras/lista-de-compras-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { BodyComponent } from './modules/body/body.component';
+import { FooterComponent } from './modules/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
